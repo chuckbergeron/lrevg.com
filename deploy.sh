506f0099f02bc9d3948c90f672236a38daa0a0c8
Charles Bergeron <chuck.bergeron@gmail.com>
2016-07-26 23:50:55 -0700

    deployed new version

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,19 @@ read -rsp $'... Press any key to continue or control-c to cancel...\n';
 
 export JEKYLL_ENV=production
 
-git checkout 'source'
+git checkout 'master'
 bundle exec jekyll build
 git add .
 git commit -a -m "deployed new version"
-git push origin 'source'
+git push origin 'master'
 
 rm -rf /tmp/_site
 cp -r _site /tmp/_site
-git checkout 'master'
+git checkout 'gh-pages'
 cp -r /tmp/_site/* .
 git add .
 git commit -a -m "deployed new version"
-git push -f origin 'master'
-git checkout 'source'
+git push -f origin 'gh-pages'
+git checkout 'master'
 
 export JEKYLL_ENV=development
